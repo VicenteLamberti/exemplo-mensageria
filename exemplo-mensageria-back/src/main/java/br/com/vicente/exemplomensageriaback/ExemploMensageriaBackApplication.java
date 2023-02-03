58173a56fc33e41d48d47152349bb60bb558f7b2
Vicente Leonardo De Souza Lamberti <vicente.lamberti@hotmail.com>
2023-02-03 08:46:36 -0300

    Criação do projeto

---
+++ exemplo-mensageria-back/src/main/java/br/com/vicente/exemplomensageriaback/ExemploMensageriaBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.vicente.exemplomensageriaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploMensageriaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploMensageriaBackApplication.class, args);
+	}
+
+}
---
+++ exemplo-mensageria-back/src/test/java/br/com/vicente/exemplomensageriaback/ExemploMensageriaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vicente.exemplomensageriaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploMensageriaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
