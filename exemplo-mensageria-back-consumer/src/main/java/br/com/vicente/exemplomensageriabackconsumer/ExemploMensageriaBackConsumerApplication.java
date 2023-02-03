5faccf0436032f59d9cf9c246bc09aa4b50bf7af
Vicente Leonardo De Souza Lamberti <vicente.lamberti@hotmail.com>
2023-02-03 10:43:02 -0300

    Criação projeto consumer

---
+++ exemplo-mensageria-back-consumer/src/main/java/br/com/vicente/exemplomensageriabackconsumer/ExemploMensageriaBackConsumerApplication.java
@@ -0,0 +1,13 @@
+package br.com.vicente.exemplomensageriabackconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploMensageriaBackConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploMensageriaBackConsumerApplication.class, args);
+	}
+
+}
---
+++ exemplo-mensageria-back-consumer/src/test/java/br/com/vicente/exemplomensageriabackconsumer/ExemploMensageriaBackConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vicente.exemplomensageriabackconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploMensageriaBackConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
